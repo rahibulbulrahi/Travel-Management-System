b6ddda84ea04d8114984af81e0770da772e92b3b
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-13 19:02:18 +0600

    13-9-2020    1

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Travel - Home page</title>
+        <link rel="stylesheet" type="text/css" href="style/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    </head>
+    <body>
+        <header>
+            <a href="#">Easy Tour & Travel</a>
+            <div class="menu-toggle"></div>
+            <nav>
+                <ul>
+                    <li><a href="#section-1">Home</a></li>
+                    <li><a href="#section-2">About</a></li>
+                    <li><a href="#section-3">Top Tour</a></li>
+                    <li><a href="#section-4">Destination</a></li>
+                    <li><a href="#section-5">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 <html>
     <head>
         <title>Travel - Home page</title>
-        <link rel="stylesheet" href="style/index.css">
+        <link rel="stylesheet" type="text/css" href="style/index.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     </head>
     <body>
-        <section class="header">
-            <div class="container">
-                <button type="button" class="login-btn">Log In</button>
-            </div>
-        </section>
+        <?php include('header.php')?>
+
+        Hello Welcome
     </body>
 </html>
