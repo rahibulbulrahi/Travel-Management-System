b241b750440d3139dfa6475a751085e04ffed5fb
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-13 19:09:11 +0600

    2

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Travel - Home page</title>
-        <link rel="stylesheet" type="text/css" href="style/index.css">
+        <link rel="stylesheet" type="text/css" href="style/header.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
@@ -13,7 +13,7 @@
             <div class="menu-toggle"></div>
             <nav>
                 <ul>
-                    <li><a href="#section-1">Home</a></li>
+                    <li><a href="#section-1" class="active">Home</a></li>
                     <li><a href="#section-2">About</a></li>
                     <li><a href="#section-3">Top Tour</a></li>
                     <li><a href="#section-4">Destination</a></li>
