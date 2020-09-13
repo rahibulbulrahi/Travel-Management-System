506a9a0e0515bfd98deaf772d0250f4668ccdb5e
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-13 19:43:26 +0600

    1

--- header.php
+++ header.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Travel - Home page</title>
+        <title>Home Page</title>
         <link rel="stylesheet" type="text/css" href="style/header.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
-    <body>
         <header>
             <a href="#">Easy Tour & Travel</a>
-            <div class="menu-toggle"></div>
-            <nav>
-                <ul>
-                    <li><a href="#section-1" class="active">Home</a></li>
-                    <li><a href="#section-2">About</a></li>
-                    <li><a href="#section-3">Top Tour</a></li>
-                    <li><a href="#section-4">Destination</a></li>
-                    <li><a href="#section-5">Contact</a></li>
-                </ul>
-            </nav>
+            <div class="manu-toggle">
+                <nav>
+                    <ul>
+                        <li><a href="#section-1">Home</a></li>
+                        <li><a href="#section-2">About</a></li>
+                        <li><a href="#section-3">Top Tour</a></li>
+                        <li><a href="#section-4">Destination</a></li>
+                        <li><a href="#section-5">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
+    <body>
+    
     </body>
 </html>
---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="style/home.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+        <?php include('header.php')?>
+    <body>
+    
+    </body>
+</html>
