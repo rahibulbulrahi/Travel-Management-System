a11f4b78c2763ab5ac388dc6d3eb9a8e615fc633
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-17 11:34:07 +0600

    1

--- index.php
+++ index.php
@@ -4,9 +4,32 @@
     <title>Easy Travel - Home Page</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="style/index.css">
   </head>
   <body>
-    
+    <div class="main-container" id="home">
+      <div class="navbar">
+        <div class="logo">
+          <a href="#">Easy Tour</a>
+        </div>
+        <div class="navbar_items">
+          <ul>
+            <li><a href="#"></a>Home</li>
+            <li><a href="#"></a>About</li>
+            <li><a href="#"></a>Services</li>
+            <li><a href="#"></a>Destination</li>
+            <li><a href="#"></a>contact</li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div class="banner_image">
+        <div class="banner_content">
+          <h1>Title<span>Title 2</span></h1>
+          <p>Sub Title</p>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
