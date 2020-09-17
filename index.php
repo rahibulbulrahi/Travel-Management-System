4d30b1d526bb2638d223a5e0996844d61827d5ca
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-17 12:10:17 +0600

    3

--- index.php
+++ index.php
@@ -14,11 +14,12 @@
         </div>
         <div class="navbar_items">
           <ul>
-            <li><a href="#"></a>Home</li>
-            <li><a href="#"></a>About</li>
-            <li><a href="#"></a>Services</li>
-            <li><a href="#"></a>Destination</li>
-            <li><a href="#"></a>contact</li>
+            <li><a href="#home"></a>Home</li>
+            <li><a href="#about"></a>About</li>
+            <li><a href="#service"></a>Services</li>
+            <li><a href="#destination"></a>Destination</li>
+            <li><a href="#contact"></a>contact</li>
+            <li><a href="#login"></a>Log in</li>
           </ul>
         </div>
       </div>
@@ -26,8 +27,46 @@
 
       <div class="banner_image">
         <div class="banner_content">
-          <h1>Title</h1>
-          <p>Sub Title</p>
+          <h1>Get Away On This Weekend</h1>
+          <p>TO THE HEAVEN BEACH</p>
+        </div>
+      </div>
+      <div class="about" id="about">
+        <h1 class="title">About Us</h1>
+        <p class="about_para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+          Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing 
+          and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining 
+          essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
+          versions of Lorem Ipsum.</p>
+      </div>
+
+      <div class="services" id="service"></div>
+      <h1 class="title">Our Survices</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt delectus neque similique asperiores sequi, ea nobis ratione maxime quisquam repellat soluta sunt consectetur dicta quod rem voluptatibus atque earum aliquid culpa expedita numquam molestias perspiciatis temporibus. Neque reiciendis, dolor fugit quisquam eligendi aut dignissimos adipisci, incidunt, molestiae in nisi id?</p>
+
+      <div class="diff_services">
+        <div class="diff_service_item">
+          <img src="images/hotel.jpg" alt="service_image">
+          <h3>Hotel Booking</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae sint laborum aperiam, minima necessitatibus quaerat? Perferendis magnam cupiditate vitae.</p>
+        </div>
+        <div class="diff_service_item">
+        <img src="images/hotel.jpg" alt="service_image">
+          <h3>Hotel Booking</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae sint laborum aperiam, minima necessitatibus quaerat? Perferendis magnam cupiditate vitae.</p>
+        </div>
+        <div class="diff_service_item">
+        <img src="images/hotel.jpg" alt="service_image">
+          <h3>Hotel Booking</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae sint laborum aperiam, minima necessitatibus quaerat? Perferendis magnam cupiditate vitae.</p>
         </div>
       </div>
     </div>
