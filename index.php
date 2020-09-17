b7d8d1bf0fec23a49b90f2b3add09d5751cf663a
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-17 21:53:09 +0600

    11

--- index.php
+++ index.php
@@ -9,21 +9,19 @@
   <body>
     <div class="main-container" id="home">
       <!--Navbar Start-->
-      <div class="navbar">
-        <div class="logo">
-          <a href="#">Easy Tour</a>
-        </div>
-        <div class="navbar_items">
-          <ul>
+      <nav>
+        <ul>
+          <li class="logo">Easy Tour</li>
+          <div class="items">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#service">Services</a></li>
             <li><a href="#destination">Destination</a></li>
             <li><a href="#contact">contact Us</a></li>
             <li><a href="#login">Log in</a></li>
-          </ul>
-        </div>
-      </div>
+          </div>
+        </ul>
+      </nav>
       <!--Navbar Finish-->
       <!--Banner-->
       <div class="banner_image">
@@ -131,6 +129,9 @@
         <a href="#">@ Easy Travel</a>
       </div>
       <!--Footer Finish-->
+      <div class="arrow">
+        <a href="#home"><img src="images/arrow.png" alt="up_arrow"></a>
+      </div>
     </div>
   </body>
 </html>
