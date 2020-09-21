d0d10f57b8eeb5c8c83504e06ba14882150fe711
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-09-21 10:08:56 +0600

    2

--- hotel.php
+++ hotel.php
@@ -99,5 +99,21 @@
 		</div>
 	</div>
     <!--Booking Bottom Finish-->
+    <!--about -->
+ 	<div class="about-wthree" id="about">
+		  <div class="container">
+				   <div class="ab-w3l-spa">
+                            <h3 class="title-w3-agileits title-black-wthree">About Our All Hotels</h3> 
+						   <p class="about-para-w3ls">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Sed tempus vestibulum lacus blandit faucibus. Nunc imperdiet, diam nec rhoncus ullamcorper, nisl nulla suscipit ligula, at imperdiet urna</p>
+						   <img src="images/about.jpg" class="img-responsive" alt="Hair Salon">
+										<div class="w3l-slider-img">
+											<img src="images/a1.jpg" class="img-responsive" alt="Hair Salon">
+										</div>
+                                       
+		          </div>
+		   	<div class="clearfix"> </div>
+         </div>
+    </div>
+ 	<!--about Finish-->
 </body>
 </html>
